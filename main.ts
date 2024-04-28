2f7d158f23deced1755249b59ae63ac5b00feae7
MaheenSheikh <maheensheikh865@gmail.com>
2024-04-28 18:56:44 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,18 @@
+// let firstname = "Maheen";
+// let profession ="programmer" ;
+
+// console.log(`Hello my name is ${firstname} and my profession is ${profession}`)
+
+//  let index = 10;
+//  let result = ++index + ++index + 6 + --index - 2;
+//  console.log(result)
+ 
+// let username="Maheen"
+// console.log(username)
+
+
+
+let num1=30
+let num2=20
+let sum= num1 + num2
+console.log(sum) 
